92838d975b092b8b1fa4f7042c8824a8b46fedb9
surferzer <mafernandazr@gmail.com>
2019-10-03 17:41:00 -0500

    se imprimen items seleccionados del submenu en pantalla negra

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -4,22 +4,35 @@ import './Board.css';
 
 //function Board () {
 class Board extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props)
 	}
-	render () {
-		return(
+	render() {
+		return (
 			<div className="orderSection">
 				<button className="table">MESA</button>
-				<div className="order">{this.props.selectedItem.name}  {this.props.selectedItem.price}</div>
-				
+
+				<div className="order" >
+					{this.props.selectedItem.map((element) => {
+						console.log(element.name, element.price)
+
+						//key=element.key		 	
+						return (<li>{element.name}</li>)
+					})
+
+					}
+
+
+
+				</div>
+
 				<div className="orderButtons">
 					<button className="kitchenAndBar">ENVIAR A COCINA</button>
 					<button className="check">IMPRIMIR CUENTA</button>
 				</div>
-				</div>
+			</div>
 
-				
+
 		);
 	}
 }
@@ -29,3 +42,4 @@ export default Board;
 
 
 
+//<div className="order">{this.props.selectedItem.name}  {this.props.selectedItem.price}</div>
