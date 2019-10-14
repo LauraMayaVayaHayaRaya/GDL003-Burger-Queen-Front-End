6b8f26488a65e73661ad51a0ba326733827f4c83
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-10-14 15:25:25 -0500

    Updates

---
+++ precache-manifest.b91c06f64cc825e12814592fbd8842d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdcb071f031882b729f7e513d8aef0b",
+    "url": "/GDL003-Burger-Queen-Front-End/index.html"
+  },
+  {
+    "revision": "bcb35aa77e7634f8f940",
+    "url": "/GDL003-Burger-Queen-Front-End/static/css/main.44eacd3a.chunk.css"
+  },
+  {
+    "revision": "aca11bf550e0dfe83b33",
+    "url": "/GDL003-Burger-Queen-Front-End/static/js/2.aa5ba728.chunk.js"
+  },
+  {
+    "revision": "bcb35aa77e7634f8f940",
+    "url": "/GDL003-Burger-Queen-Front-End/static/js/main.7a90f6dd.chunk.js"
+  },
+  {
+    "revision": "6913d5ad1567ec221455",
+    "url": "/GDL003-Burger-Queen-Front-End/static/js/runtime-main.22a69edc.js"
+  },
+  {
+    "revision": "416b185c0dd770fec21112ef0cd3e877",
+    "url": "/GDL003-Burger-Queen-Front-End/static/media/logo-blanco.416b185c.svg"
+  },
+  {
+    "revision": "a91949a2fe843bfc91beb48571dbaa82",
+    "url": "/GDL003-Burger-Queen-Front-End/static/media/logov5.a91949a2.svg"
+  }
+]);
